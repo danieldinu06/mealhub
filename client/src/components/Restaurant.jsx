234668eda501618014e0f3f3fb128933b6fc212d
freeDaTitty <danieldinu6091@gmail.com>
2022-07-08 11:17:33 +0300

    Refactored frontend

--- client/src/components/Restaurant.jsx
+++ client/src/components/Restaurant.jsx
@@ -30,10 +30,70 @@ export default function Restaurant() {
 
 
     return(
-        <div>
-            {drinks.map(meal => (
-                <h1>{meal.name}</h1>
-            ))}
+        <div className={"grid gap-10"}>
+            <div>
+                <p className={"mb-5 font-bold text-3xl"}>
+                    Type of meals
+                </p>
+                <div className={"grid grid-cols-3 gap-2"}>
+                    {meals.map(meal => (
+                        <div className={"grid border-2 border-black rounded-md"} key={meal.id}>
+                            <div className={"grid grid-cols-2"}>
+                                <img src={meal.image} alt="No meal" className={"max-h-40"}/>
+                                <div className="grid grid-rows-2">
+                                    <p>
+                                        {meal.name}
+                                    </p>
+                                    <p>
+                                        Ingredients
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className={"grid grid-cols-2 h-12"}>
+                                <p>
+                                    Price
+                                </p>
+                                <button className={"bg-orange-400 rounded-md"}>
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <p className={"mb-5 font-bold text-3xl"}>
+                    Type of drinks
+                </p>
+                <div className={"grid grid-cols-3 gap-2"}>
+                    {drinks.map(drink => (
+                        <div className={"grid border-2 border-black rounded-md"} key={drink.id}>
+                            <div className={"grid grid-cols-2"}>
+                                <img src={drink.image} alt="No meal" className={"max-h-40"}/>
+                                <div className="grid grid-rows-2">
+                                    <p>
+                                        {drink.name}
+                                    </p>
+                                    <p>
+                                        Ingredients
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className={"grid grid-cols-2 h-12"}>
+                                <p>
+                                    Price
+                                </p>
+                                <button className={"bg-orange-400 rounded-md"}>
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
+
     );
 }
