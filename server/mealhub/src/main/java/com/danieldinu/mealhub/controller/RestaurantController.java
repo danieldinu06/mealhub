675e9aa09c3a508d78ad774ed2c1730f3360451a
freeDaTitty <danieldinu6091@gmail.com>
2022-09-19 15:27:34 +0300

    Updated Restaurant Controller

--- server/mealhub/src/main/java/com/danieldinu/mealhub/controller/RestaurantController.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/controller/RestaurantController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/restaurant")
+@RequestMapping(path = "/restaurants")
 @CrossOrigin
 public class RestaurantController {
     private final RestaurantService restaurantService;
