7111fa22ad9710f89bdbd9744830c2cde32e304a
freeDaTitty <danieldinu6091@gmail.com>
2022-09-19 15:23:30 +0300

    Updated User Repository

--- server/mealhub/src/main/java/com/danieldinu/mealhub/repository/UserRepository.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String username);
+    Boolean existsByName(String username);
+    Boolean existsByEmail(String username);
 }
