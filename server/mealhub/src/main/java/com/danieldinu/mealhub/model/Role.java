37e23405825fb2a85118e616accaa19a4b682b94
freeDaTitty <danieldinu6091@gmail.com>
2022-09-19 15:25:14 +0300

    Added Role class

---
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/Role.java
@@ -0,0 +1,34 @@
+package com.danieldinu.mealhub.model;
+
+import com.danieldinu.mealhub.model.utils.RoleType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    @NonNull
+    private RoleType roleType;
+}
