3bace682f03fb3617bc33689f7774fcb8f8bc6b6
freeDaTitty <danieldinu6091@gmail.com>
2022-09-19 15:22:19 +0300

    Refactored Order Service

--- server/mealhub/src/main/java/com/danieldinu/mealhub/service/OrderService.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.danieldinu.mealhub.service;
 
+import com.danieldinu.mealhub.model.Drink;
+import com.danieldinu.mealhub.model.Meal;
 import com.danieldinu.mealhub.model.Order;
 import com.danieldinu.mealhub.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +24,10 @@ public class OrderService {
     }
 
     public Optional<Order> getOrder(Long id) {
-        return orderRepository.findById(id);
+        Optional<Order> order = orderRepository.findById(id);
+
+        order.get().setPrice(calculateOrderPrice(order.get()));
+        return order;
     }
 
     public List<Order> getAllOrders() {
@@ -31,5 +36,25 @@ public class OrderService {
 
     public void deleteOrder(Long id) {
         orderRepository.deleteById(id);
+    }
+
+    private Double calculateOrderPrice(Order order) {
+        List<Meal> meals = order.getMeals();
+        List<Drink> drinks = order.getDrinks();
+        Double price = 0.0;
+
+        if (!meals.isEmpty()) {
+            for (Meal meal : meals) {
+                price += meal.getPrice();
+            }
+        }
+
+        if (!drinks.isEmpty()) {
+            for (Drink drink : drinks) {
+                price += drink.getPrice();
+            }
+        }
+
+        return price;
     }
 }
