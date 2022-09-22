ada432b1276cc8cde5c09e7882e16f775145b814
freeDaTitty <danieldinu6091@gmail.com>
2022-09-23 00:13:24 +0300

    Added role types for users

--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/utils/RoleType.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/utils/RoleType.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.GrantedAuthority;
 public enum RoleType implements GrantedAuthority {
     ROLE_ADMIN, ROLE_USER;
 
-
     @Override
     public String getAuthority() {
         return name();
