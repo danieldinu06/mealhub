f60514a69fb18ce502d6c470cb60fa28f0672854
freeDaTitty <danieldinu6091@gmail.com>
2022-09-23 00:27:03 +0300

    Refactored main component for routing

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,58 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tw-elements';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Checkout from "./components/Checkout";
+import Header from "./components/fixedComponents/Header";
+import Footer from "./components/fixedComponents/Footer";
+import RestaurantsPage from "./components/pages/RestaurantsPage";
+import RestaurantPage from "./components/pages/RestaurantPage";
+import PrivateRoutes from "./components/utils/PrivateRoutes";
+import Login from "./components/pages/Login";
+import ProtectedRoutes from "./components/utils/ProtectedRoutes";
+import PublicRoutes from "./components/utils/PublicRoutes";
+import Register from "./components/pages/Register";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<PublicRoutes/>}>
+                    <Route path={"/restaurants"} element={(
+                        <>
+                            <Header />
+                            <RestaurantsPage/>
+                            <Footer />
+                        </>
+                    )}/>
+                    <Route path={"/restaurants/:id"} element={(
+                        <>
+                            <Header/>
+                            <RestaurantPage/>
+                            <Footer/>
+                        </>
+                    )}/>
+                </Route>
+
+                <Route element={<ProtectedRoutes/>}>
+                    <Route path={"/login"} element={(<Login/>)}/>
+                    <Route path={"/register"} element={(<Register/>)}/>
+                </Route>
+
+                <Route element={<PrivateRoutes/>}>
+                    <Route path={"/checkout"} element={(
+                        <>
+                            <Header/>
+                            <Checkout/>
+                            <Footer/>
+                        </>
+                    )}/>
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
