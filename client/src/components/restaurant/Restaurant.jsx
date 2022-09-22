42bdc551e44a3437180077a0e27f790a82440b45
freeDaTitty <danieldinu6091@gmail.com>
2022-09-23 00:21:30 +0300

    Refactored Restaurant component

--- client/src/components/restaurant/Restaurant.jsx
+++ client/src/components/restaurant/Restaurant.jsx
@@ -1,64 +1,11 @@
-import React, {useEffect, useState} from 'react'
-import axios from "axios";
-import {useParams} from "react-router-dom";
 import './Restaurant.css';
-import Meal from "./Meal";
-import Drink from "./Drink";
+import Meal from "../victuals/Meal";
+import Drink from "../victuals/Drink";
 
 export default function Restaurant() {
 
-    const { id } = useParams();
-    const [restaurant, setRestaurant] = useState([]);
-    const [mealTypes, setMealTypes] = useState([]);
-    const [drinks, setDrinks] = useState([]);
-    const [drinkTypes, setDrinkTypes] = useState([]);
-
-    useEffect(() => {
-        const restaurantUrl = `http://localhost:8888/api/public/restaurants/${id}`;
-        const drinksUrl = restaurantUrl + '/drinks';
-
-        axios.get(restaurantUrl)
-            .then(response => setRestaurant(response.data))
-            .catch(error => console.log(error));
-
-        axios.get('http://localhost:8888/meals')
-            .then(response => setMealTypes(response.data))
-            .catch(error => console.log(error));
-
-        axios.get(drinksUrl)
-            .then(response => setDrinks(response.data))
-            .catch(error => console.log(error));
-
-        axios.get('http://localhost:8888/drinks')
-            .then(response => setDrinkTypes(response.data))
-            .catch(error => console.log(error));
-
-    }, [id]);
-
-
     return(
         <div className={"centerPanel"}>
-            <catalogue>
-                {
-                    mealTypes.map(
-                        (mealType, index) => (
-                            <div key={index} className={"mealType"}>
-                                {mealType}
-                            </div>
-                        )
-                    )
-                }
-                {
-                    drinkTypes.map(
-                        (drinkType, index) => (
-                            <div key={index} className={"drinkType"}>
-                                {drinkType}
-                            </div>
-                        )
-                    )
-                }
-            </catalogue>
-
             <div className={"grid gap-10"}>
                 <Meal/>
                 <Drink/>
