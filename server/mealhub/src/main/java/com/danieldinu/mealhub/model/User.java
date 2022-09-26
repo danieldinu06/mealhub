9651ea7ef6475c2caa000e4cdba91556660d1fe1
freeDaTitty <danieldinu6091@gmail.com>
2022-09-26 10:51:19 +0300

    Refactored User model

--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/User.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/User.java
@@ -49,7 +49,7 @@ public class User {
     @NonNull
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(	name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
