8552a138aa1a7ec1f542422def2ae36b644fcdb4
freeDaTitty <danieldinu6091@gmail.com>
2022-09-26 10:45:56 +0300

    Refactored App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,8 @@
 import './App.css';
 import React from "react";
-import {useEffect, useState} from "react";
-import AuthService from "./services/auth.service";
 
 
 function App() {
-    const [showCheckout, setShowCheckout] = useState(false);
-    const [currentUser, setCurrentUser] = useState(undefined);
-
-    useEffect(() => {
-        const user = AuthService.getCurrentUser();
-
-        if (user) {
-            setCurrentUser(user);
-            setShowCheckout(user.roles.includes("ROLE_USER"));
-        }
-    }, []);
-
-    const logOut = () => {
-        AuthService.logout();
-    }
-
     return (
         <div className="App">
             <div className="AppContainer">
