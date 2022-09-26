be1a620acb9a62caa11959ca72269230cdfe1b41
freeDaTitty <danieldinu6091@gmail.com>
2022-09-26 10:50:29 +0300

    Changed OrderController path

--- server/mealhub/src/main/java/com/danieldinu/mealhub/controller/OrderController.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/controller/OrderController.java
@@ -2,10 +2,17 @@ package com.danieldinu.mealhub.controller;
 
 import com.danieldinu.mealhub.model.Order;
 import com.danieldinu.mealhub.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -17,6 +24,7 @@ import java.util.Optional;
 public class OrderController {
     private final OrderService orderService;
 
+    @Autowired
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
@@ -33,7 +41,6 @@ public class OrderController {
     }
 
     @GetMapping(path = "/{order_id}")
-    @PreAuthorize("hasRole('ADMIN')")
     @ResponseBody
     public ResponseEntity<Order> getOrder(@PathVariable(value = "order_id") Long id) {
         Optional<Order> order = orderService.getOrder(id);
