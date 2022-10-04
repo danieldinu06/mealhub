30218cce7db4ff503263f99d8e5fda144cdfe72b
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-04 15:54:06 +0300

    Refactored UserDatailsServiceImpl to use UserService

--- server/mealhub/src/main/java/com/danieldinu/mealhub/security/services/UserDetailsServiceImpl.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/security/services/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.danieldinu.mealhub.security.services;
 
 import com.danieldinu.mealhub.model.User;
 import com.danieldinu.mealhub.repository.UserRepository;
+import com.danieldinu.mealhub.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,16 +11,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByName(username);
+        User user = userService.findByName(username);
 
         if (user == null) {
             throw new UsernameNotFoundException("User '" + username + "' not found");
