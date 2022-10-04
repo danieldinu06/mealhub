f7e0484b6b158fd99b37153d69d205176614ff6d
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-04 15:52:09 +0300

    Added more functions to RoleService

--- server/mealhub/src/main/java/com/danieldinu/mealhub/service/RoleService.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/service/RoleService.java
@@ -1,11 +1,13 @@
 package com.danieldinu.mealhub.service;
 
 import com.danieldinu.mealhub.model.Role;
+import com.danieldinu.mealhub.model.utils.RoleType;
 import com.danieldinu.mealhub.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleService {
@@ -22,5 +24,9 @@ public class RoleService {
 
     public void addRoles(List<Role> roles) {
         roleRepository.saveAll(roles);
+    }
+
+    public Optional<Role> findByRoleType(RoleType roleType) {
+        return roleRepository.findByRoleType(roleType);
     }
 }
