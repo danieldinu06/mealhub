9c4ffd2044786026ff681eec65a1601e7e54b885
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-06 20:50:16 +0300

    Added repository for DrinkOrderElement

---
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/repository/DrinkOrderElementRepository.java
@@ -0,0 +1,7 @@
+package com.danieldinu.mealhub.repository;
+
+import com.danieldinu.mealhub.model.DrinkOrderElement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DrinkOrderElementRepository extends JpaRepository<DrinkOrderElement, Long> {
+}
