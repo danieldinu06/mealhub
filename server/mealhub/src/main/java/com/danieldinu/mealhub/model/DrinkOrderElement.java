f7a5e1309e29339a1f52154bc39b506550645672
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-06 20:49:38 +0300

    Added DrinkOrderElement as an element for drinks in orders

---
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/DrinkOrderElement.java
@@ -0,0 +1,33 @@
+package com.danieldinu.mealhub.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class DrinkOrderElement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @NonNull
+    private Drink drink;
+    @NonNull
+    private Integer quantity;
+}
