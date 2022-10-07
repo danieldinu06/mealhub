2e7f20b57ea393f486562ed7b077fda11d45fa7a
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-07 08:57:32 +0300

    Added MealOrderElementRepository

---
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/repository/MealOrderElementRepository.java
@@ -0,0 +1,7 @@
+package com.danieldinu.mealhub.repository;
+
+import com.danieldinu.mealhub.model.MealOrderElement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MealOrderElementRepository extends JpaRepository<MealOrderElement, Long> {
+}
