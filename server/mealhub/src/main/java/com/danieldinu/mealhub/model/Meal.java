e0076aba9d9338c4e1b9ee8bf1a4ab07e8c097a9
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-08 20:32:53 +0300

    Refactored Meal to extend Element

--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/Meal.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/Meal.java
@@ -1,63 +1,20 @@
 package com.danieldinu.mealhub.model;
 
-import com.danieldinu.mealhub.model.utils.MealType;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import com.danieldinu.mealhub.model.baseModel.Element;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Table(name = "meals")
 @Entity
+@SuperBuilder
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@RequiredArgsConstructor
-public class Meal {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(
-            updatable = false
-    )
-    private Long id;
-
-    @Column(
-            columnDefinition = "TEXT"
-    )
-    @NonNull
-    private String name;
-
-    @Column(
-            columnDefinition = "TEXT"
-    )
-    @NonNull
-    private String image;
-
-    @Column(
-            columnDefinition = "TEXT"
-    )
-    @NonNull
-    private String ingredients;
-
-    @NonNull
-    private Double price;
-
-    private Double discount;
-
-    @NonNull
-    private MealType type;
-
-    @ManyToMany(mappedBy = "meals")
-    @JsonIgnore
-    private Set<Restaurant> restaurants = new HashSet<>();
-
-    @ManyToMany(mappedBy = "meals")
-    @JsonIgnore
-    private Set<Order> order = new HashSet<>();
-
-    public void addRestaurant(Restaurant restaurant) {
-        restaurants.add(restaurant);
-    }
+public class Meal extends Element {
 }
