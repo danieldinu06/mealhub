335bf7c9f51c69a87850e61c1e1e5ebca9adfad2
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-08 20:33:08 +0300

    Refactored DrinkrOrderElement to extend OrderElement

--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/DrinkOrderElement.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/DrinkOrderElement.java
@@ -1,33 +1,38 @@
 package com.danieldinu.mealhub.model;
 
-import lombok.AllArgsConstructor;
+import com.danieldinu.mealhub.model.baseModel.OrderElement;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import java.util.List;
 
 @Entity
+@SuperBuilder
+@NoArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
-@RequiredArgsConstructor
-@NoArgsConstructor
-public class DrinkOrderElement {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+public class DrinkOrderElement extends OrderElement{
     @OneToOne
     @NonNull
     private Drink drink;
 
-    @NonNull
-    private Integer quantity;
+    public static void addDrink(Drink drink, List<DrinkOrderElement> orderElements) {
+        boolean hasElement = false;
+
+        for (DrinkOrderElement orderElement : orderElements) {
+            if (orderElement.getDrink().equals(drink)) {
+                orderElement.setQuantity(orderElement.getQuantity() + 1);
+                hasElement = true;
+            }
+        }
+
+        if (!hasElement) {
+            orderElements.add(DrinkOrderElement.builder().drink(drink).quantity(1).build());
+        }
+    }
 }
