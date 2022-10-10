7bc71d2ce937cff92d744f667434cc3690f57d6a
danieldinu06 <dinudaniel0604@gmail.com>
2022-10-10 15:48:50 +0300

    Refactored code in Order model

--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/Order.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/Order.java
@@ -14,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -52,7 +51,7 @@ public class Order{
     )
     private List<DrinkOrderElement> drinks = new ArrayList<>();
 
-    @ManyToMany
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "order_meal",
             joinColumns = @JoinColumn(name = "order_id"),
