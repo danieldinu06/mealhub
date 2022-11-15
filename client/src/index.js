4e2c3de39b630b2b40608285949588418a78ca55
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-15 21:42:00 +0200

    Added more routes to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tw-elements';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Checkout from "./components/Checkout";
-import Header from "./components/fixedComponents/Header";
-import Footer from "./components/fixedComponents/Footer";
-import RestaurantsPage from "./components/pages/RestaurantsPage";
-import RestaurantPage from "./components/pages/RestaurantPage";
+import Checkout from "./components/payment/Checkout";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import RestaurantsPage from "./components/restaurant/RestaurantsPage";
+import RestaurantPage from "./components/restaurant/RestaurantPage";
 import PrivateRoutes from "./components/utils/PrivateRoutes";
-import Login from "./components/pages/Login";
+import Login from "./components/page/Login";
 import ProtectedRoutes from "./components/utils/ProtectedRoutes";
 import PublicRoutes from "./components/utils/PublicRoutes";
-import Register from "./components/pages/Register";
-import VerificationPage from "./components/pages/VerificationPage";
+import Register from "./components/page/Register";
+import VerificationPage from "./components/page/VerificationPage";
+import HomePage from "./components/page/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -23,6 +24,12 @@ root.render(
         <BrowserRouter>
             <Routes>
                 <Route element={<PublicRoutes/>}>
+                    <Route path={"/"} element={(
+                        <>
+                            <HomePage/>
+                            <Footer/>
+                        </>
+                    )}/>
                     <Route path={"/restaurants"} element={(
                         <>
                             <Header/>
