cade03edf1a0337fa44ef684bc2ace32c42f38b4
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-15 21:42:53 +0200

    Refactored urls in a file

---
+++ client/src/utils/urls.js
@@ -0,0 +1,3 @@
+export const RESTAURANT_URL = "http://localhost:8888/api/public/restaurants";
+export const ORDER_URL = "http://localhost:8888/api/private/checkout";
+export const API_URL = "http://localhost:8888/api/public/auth";
