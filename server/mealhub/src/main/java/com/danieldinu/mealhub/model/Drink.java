deeb23f4b3400dea0db8f8c028f1e3a8659d1571
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 16:00:22 +0200

    Refactored victual models

--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/Drink.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/Drink.java
@@ -1,19 +1,73 @@
 package com.danieldinu.mealhub.model;
 
-import com.danieldinu.mealhub.model.baseModel.Element;
+import com.danieldinu.mealhub.model.utils.DrinkType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
 @Table(name = "drinks")
 @Entity
-@SuperBuilder
-@NoArgsConstructor
 @Getter
 @Setter
-public class Drink extends Element {
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class Drink {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NonNull
+    private String name;
+
+    @Column(
+            columnDefinition = "TEXT"
+    )
+    @NonNull
+    private String image;
+
+    @NonNull
+    private Double price;
+
+    private Double discount;
+
+    @NonNull
+    private DrinkType type;
+
+    @ManyToMany(mappedBy = "drinks")
+    @JsonIgnore
+    @Builder.Default
+    private Set<Restaurant> restaurants = new HashSet<>();
+
+    @ManyToMany(mappedBy = "drinks")
+    @JsonIgnore
+    @Builder.Default
+    private Set<Order> order = new HashSet<>();
+
+    public void addRestaurant(Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
 }
--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/Meal.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/Meal.java
@@ -1,20 +1,82 @@
 package com.danieldinu.mealhub.model;
 
-import com.danieldinu.mealhub.model.baseModel.Element;
+import com.danieldinu.mealhub.model.utils.MealType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Table(name = "meals")
 @Entity
-@SuperBuilder
-@NoArgsConstructor
 @Getter
 @Setter
-public class Meal extends Element {
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            columnDefinition = "TEXT"
+    )
+    @NonNull
+    private String name;
+
+    @Column(
+            columnDefinition = "TEXT"
+    )
+    @NonNull
+    private String image;
+
+    @Column(
+            columnDefinition = "TEXT"
+    )
+    @NonNull
+    private String ingredients;
+
+    @NonNull
+    private Double price;
+
+    private Double discount;
+
+    @NonNull
+    private MealType type;
+
+    @OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER, mappedBy = "meal")
+    private List<MealOrderElement> mealOrderElement;
+
+    @ManyToMany(mappedBy = "meals")
+    @JsonIgnore
+    @Builder.Default
+    private Set<Restaurant> restaurants = new HashSet<>();
+
+    public void addRestaurant(Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
 }
