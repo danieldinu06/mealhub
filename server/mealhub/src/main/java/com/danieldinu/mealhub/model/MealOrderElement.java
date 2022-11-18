254d6a453cbd3fef28952b0e9c12d41d1dbdbca4
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 16:00:35 +0200

    Refactored MealOrderElement model

--- server/mealhub/src/main/java/com/danieldinu/mealhub/model/MealOrderElement.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/model/MealOrderElement.java
@@ -3,13 +3,13 @@ package com.danieldinu.mealhub.model;
 import com.danieldinu.mealhub.model.baseModel.OrderElement;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-import java.util.List;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Entity
 @SuperBuilder
@@ -17,22 +17,9 @@ import java.util.List;
 @Getter
 @Setter
 public class MealOrderElement extends OrderElement{
-    @OneToOne
-    @NonNull
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Meal meal;
 
-    public static void addMeal(Meal meal, List<MealOrderElement> orderElements) {
-        boolean hasElement = false;
-
-        for (MealOrderElement orderElement : orderElements) {
-            if (orderElement.getMeal().equals(meal)) {
-                orderElement.setQuantity(orderElement.getQuantity() + 1);
-                hasElement = true;
-            }
-        }
-
-        if (!hasElement) {
-            orderElements.add(MealOrderElement.builder().meal(meal).quantity(1).build());
-        }
-    }
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public Order order;
 }
