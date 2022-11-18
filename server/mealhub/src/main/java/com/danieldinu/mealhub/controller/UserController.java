ae5fccef50bd17b8af5596855d2ec12f14298149
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 15:58:14 +0200

    Implemented UserController

---
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.danieldinu.mealhub.controller;
+
+import com.danieldinu.mealhub.model.User;
+import com.danieldinu.mealhub.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/private/user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public User getUser(@RequestParam String name) {
+        return userService.findByName(name);
+    }
+}
