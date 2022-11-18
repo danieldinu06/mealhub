c1c5c78bd90afe36cc57888472ea7d43e1da0a66
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 15:44:10 +0200

    Added more urls in url js

--- client/src/utils/urls.js
+++ client/src/utils/urls.js
@@ -1,3 +1,4 @@
 export const RESTAURANT_URL = "http://localhost:8888/api/public/restaurants";
 export const ORDER_URL = "http://localhost:8888/api/private/checkout";
 export const API_URL = "http://localhost:8888/api/public/auth";
+export const USER_URL = "http://localhost:8888/api/private/user"
