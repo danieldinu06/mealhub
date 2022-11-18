574581adfea4dc567c943b5cf40967ea7088dc97
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 15:46:15 +0200

    Refactored ProtectedRoutes component

--- client/src/components/utils/ProtectedRoutes.jsx
+++ client/src/components/utils/ProtectedRoutes.jsx
@@ -1,8 +1,16 @@
 import {Navigate, Outlet} from "react-router-dom";
 import AuthService from "../../services/authentication/auth.service";
+import {useEffect, useState} from "react";
 
 function ProtectedRoutes() {
-    let authToken = AuthService.getCurrentUser();
+    const [authToken, setAuthToken] = useState(null);
+
+    useEffect(() => {
+        if (AuthService.getCurrentUser()) {
+            AuthService.getCurrentUser()
+                .then(response => setAuthToken(response.data.token));
+        }
+    });
 
     return (
         !authToken ? <Outlet/> : <Navigate to={"/restaurants"}/>
