2eea410cccabe172286fc986d32d9cde631e8fdc
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 16:01:27 +0200

    Updated security

--- server/mealhub/src/main/java/com/danieldinu/mealhub/security/SecurityConfiguration.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/security/SecurityConfiguration.java
@@ -15,6 +15,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -54,7 +60,7 @@ public class SecurityConfiguration {
 
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/api/public/**", "/static/css/**", "/static/js/**", "/images/**").permitAll()
+                    .antMatchers("/api/public/**", "/api/private/checkout", "/api/private/payment/**", "/static/css/**", "/static/js/**", "/images/**").permitAll()
                     .antMatchers("/api/private/**").hasAnyRole("USER", "ADMIN")
                     .antMatchers("/**").hasRole("ADMIN")
 
@@ -90,6 +96,17 @@ public class SecurityConfiguration {
         http.addFilterBefore(new JwtAuthenticationFilter(jwtProviderService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        source.registerCorsConfiguration("/**", config.applyPermitDefaultValues());
+        config.setExposedHeaders(Arrays.asList("Authorization"));
+
+        return source;
     }
 
     @Bean
