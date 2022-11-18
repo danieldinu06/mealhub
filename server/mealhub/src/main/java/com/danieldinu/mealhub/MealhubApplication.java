60cc450adee1c611b7362eb099112fc7040617ec
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 15:59:22 +0200

    Refactored starting point class

--- server/mealhub/src/main/java/com/danieldinu/mealhub/MealhubApplication.java
+++ server/mealhub/src/main/java/com/danieldinu/mealhub/MealhubApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MealhubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MealhubApplication.class, args);
 	}
