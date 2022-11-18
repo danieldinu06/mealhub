373d0b6bf6c6ea30b3650de4d7bf7a199732662f
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 15:49:08 +0200

    Refactored Order component

--- client/src/components/payment/Order.jsx
+++ client/src/components/payment/Order.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {orderState} from "../../state";
 import {useAtom} from "jotai";
+import "./Order.css"
 
 const Order = () => {
     const [meals, ] = useAtom(orderState.order.meals);
@@ -9,6 +10,8 @@ const Order = () => {
     const [order, setOrder] = useState({meals, drinks});
 
     useEffect(() => {
+        console.log(meals);
+
         setOrder(
             prevOrder => ({
                 ...prevOrder,
@@ -22,44 +25,51 @@ const Order = () => {
     return (
         <div className="mt-20 flex flex-col gap-4">
             {order.meals.map(meal => (
-                <div className="bg-gray-200 shadow-md rounded-md grid grid-cols-2 gap-3" key={meal.id}>
-                    <div>
-                        <img src={meal.image} alt="Food"/>
-                    </div>
-                    <div className="grid grid-rows-3 gap-3">
+                <div className="bg-gray-200 h-28 shadow-md rounded-md grid grid-cols-2" key={meal.id}>
+                    <img className={"h-28"} src={meal.image} alt="Food"/>
+                    <div className="grid grid-rows-3 gap-3 elements">
                         <div>
                             {meal.name}
                         </div>
                         <div className={"font-bold"}>
-                            {meal.price} $
+                            {meal.price * meal.quantity} RON
                         </div>
-                        <div className={"text-red-700 font-bold flex justify-between"}>
-                            <div>
+                        <div className={"font-bold quantityPanel"}>
+                            <button className={"decrease"}>
+                                -
+                            </button>
+                            <div className={"quantity"}>
                                 {meal.quantity}
                             </div>
-                            {/*<button className={"mr-2"} onClick={() => handleReduce}>*/}
-                            {/*    less*/}
-                            {/*</button>*/}
+                            <button className={"increase"}>
+                                +
+                            </button>
                         </div>
                     </div>
                 </div>
             ))}
 
             {order.drinks.map(drink => (
-                <div className="bg-gray-200 shadow-md rounded-md grid grid-cols-3 gap-3" key={drink.id}>
-                    <div>
-                        <img src={drink.image} alt="Food"/>
-                    </div>
-                    <div className="grid grid-rows-2 gap-3">
+                <div className="bg-gray-200 h-28 shadow-md rounded-md grid grid-cols-2" key={drink.id}>
+                    <img className={"h-28"} src={drink.image} alt="Food"/>
+                    <div className="grid grid-rows-3 gap-3 elements">
                         <div>
                             {drink.name}
                         </div>
-                        <div>
-                            {drink.quantity}
+                        <div className={"font-bold"}>
+                            {drink.price * drink.quantity} RON
                         </div>
-                    </div>
-                    <div>
-                        {drink.price}
+                        <div className={"font-bold quantityPanel"}>
+                            <button className={"decrease"}>
+                                -
+                            </button>
+                            <div className={"quantity"}>
+                                {drink.quantity}
+                            </div>
+                            <button className={"increase"}>
+                                +
+                            </button>
+                        </div>
                     </div>
                 </div>
             ))}
