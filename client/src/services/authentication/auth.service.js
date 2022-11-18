f957dfa6e00a363a13e1dfa5cda4669873b03bc2
danieldinu06 <dinudaniel0604@gmail.com>
2022-11-18 15:44:41 +0200

    Refactored config variables in auth.service

--- client/src/services/authentication/auth.service.js
+++ client/src/services/authentication/auth.service.js
@@ -1,11 +1,26 @@
 import axios from "axios";
-import {API_URL} from "../../utils/urls";
+import {API_URL, ORDER_URL, USER_URL} from "../../utils/urls";
+import jwt_decode from "jwt-decode";
 
+export const token = JSON.parse(localStorage.getItem("user"));
 
-export const config = {
+const loginConfig = {
     headers: {
-        "Access-Control-Allow-Origin": API_URL
+        "Access-Control-Allow-Origin": API_URL,
+        "Content-Type": "application/json"
     }
+}
+
+const config = {
+    headers: {
+        'Access-Control-Allow-Origin': USER_URL,
+        'Authorization': `Bearer ${token}`,
+    }
+}
+
+export const orderConfig = {
+    'Access-Control-Allow-Origin': ORDER_URL,
+    'Content-Type': 'application/json',
 }
 
 const register = (username, email, password) => {
@@ -19,7 +34,7 @@ const register = (username, email, password) => {
 const login = (username, password) => {
     return axios.post(API_URL + "/login", {
         username, password
-    }, config)
+    }, loginConfig)
         .then((response) => {
             if(response.data.token) {
                 localStorage.setItem("user", JSON.stringify(response.data.token));
@@ -34,7 +49,10 @@ const logout = () => {
 };
 
 const getCurrentUser = () => {
-    return JSON.parse(localStorage.getItem("user"));
+    if (!token) return null;
+
+    const decodedToken = jwt_decode(token);
+    return axios.get(USER_URL + "?name=" + decodedToken.sub, config);
 }
 
 const AuthService = {
